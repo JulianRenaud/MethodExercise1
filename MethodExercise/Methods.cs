76ebe92409312bed9506cf6efadd277a7323be90
JulianRenaud <midway6220@gmail.com>
2022-09-13 11:27:16 -0500

    Added a method

---
+++ MethodExercise/Methods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExercise1
+{
+    internal class Methods
+    {
+        public static void MethodExercise()
+        {
+            Console.WriteLine("What is your full name? (click enter to skip)");
+            var fullName = Console.ReadLine();
+
+            Console.WriteLine("What is your age? (click enter to skip)");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color? (click enter to skip)");
+            var favColor = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal at this zoo? (click enter to skip)");
+            var favAnimal = Console.ReadLine();
+
+            Console.WriteLine($"Hello {fullName}, your survey has been submitted!");
+
+
+        }
+    }
+}
--- MethodExercise/Program.cs
+++ MethodExercise/Program.cs
@@ -1,14 +1,4 @@
 ﻿
-Console.WriteLine("What is your full name? (click enter to skip)");
-var fullName = Console.ReadLine();
+using MethodExercise1;
 
-Console.WriteLine("What is your age? (click enter to skip)");
-var age = Console.ReadLine();
-
-Console.WriteLine("What is your favorite color? (click enter to skip)");
-var favColor = Console.ReadLine();
-
-Console.WriteLine("What is your favorite animal at this zoo? (click enter to skip)");
-var favAnimal = Console.ReadLine();
-
-Console.WriteLine($"Hello {fullName}, your survey has been sent!");
+Methods.MethodExercise();
