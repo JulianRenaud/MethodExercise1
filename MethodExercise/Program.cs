9fc24d58e5579497203a2f59cbfb5e3ff033d872
JulianRenaud <midway6220@gmail.com>
2022-09-13 09:17:49 -0500

    first Commit

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("What is your full name? (click enter to skip)");
+var fullName = Console.ReadLine();
+
+Console.WriteLine("What is your age? (click enter to skip)");
+var age = Console.ReadLine();
+
+Console.WriteLine("What is your favorite color? (click enter to skip)");
+var favColor = Console.ReadLine();
+
+Console.WriteLine("What is your favorite animal at this zoo? (click enter to skip)");
+var favAnimal = Console.ReadLine();
+
+Console.WriteLine($"Hello {fullName}, your survey has been sent!");
